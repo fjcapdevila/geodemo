50c5625bd96f7d2bbcf156e3fa84dfad546876f2
Francisco J. Capdevila <fjcapdevila@gmail.com>
2018-09-21 12:33:38 -0300

    Add some viewsets.

--- geodemo/providers/views.py
+++ geodemo/providers/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Provider, Language, Currency, ServiceArea
+from .serializers import ProviderSerializer, LanguageSerializer, CurrencySerializer, ServiceAreaSerializer
+
+
+class ProviderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows providers to be viewed or edited.
+    """
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+
+class LanguageViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows languages to be viewed or edited.
+    """
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+
+
+class CurrencyViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows currencies to be viewed or edited.
+    """
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
+
+
+class ServiceAreaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows service areas to be viewed or edited.
+    """
+    queryset = ServiceArea.objects.all()
+    serializer_class = ServiceAreaSerializer

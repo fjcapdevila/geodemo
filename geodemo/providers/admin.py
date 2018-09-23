40251d5193dfca78c1ee89a4d49746d3794de4da
Francisco J. Capdevila <fjcapdevila@gmail.com>
2018-09-23 18:48:05 -0300

    Use a custom admin class for Provider model.

--- geodemo/providers/admin.py
+++ geodemo/providers/admin.py
@@ -1,9 +1,14 @@
-
 from django.contrib.gis import admin
+
 from .models import Currency, Language, Provider, ServiceArea
+
+
+class ProviderAdmin(admin.ModelAdmin):
+    """Custom Admin for Providers."""
+    list_display = ('name', 'owner')
 
 
 admin.site.register(Currency)
 admin.site.register(Language)
-admin.site.register(Provider)
+admin.site.register(Provider, ProviderAdmin)
 admin.site.register(ServiceArea, admin.OSMGeoAdmin)
